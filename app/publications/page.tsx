47e9616059557c0b1a6886948a0c96272471aea7
zatyrical <zatyrical@gmail.com>
2026-09-10 00:57:44 +0800

    Complete verified source links for selected publications

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -3,16 +3,20 @@ import Navigation from '../Navigation';
 
 const publications = [
   'Venous anatomy of the superficial circumflex iliac artery perforator flap: a cadaveric and clinical study. JPRAS, 2024.',
-  'Utilizing the subunit concept to achieve better outcomes in lower limb reconstruction. PRS Global Open, 2024.',
+  'Utilizing the Subunit Concept to Achieve Better Outcomes in Lower Limb Reconstruction: A Clinical Experience in an Asian Population. PRS Global Open, 2024;12(4):e5752.',
   'Current Insights into Post-Traumatic Lymphedema. Trauma Care, 2025.',
   'The 1+X technique: A novel microsurgical approach for secondary lymphedema. JPRAS, 2025;111:1–3.',
   'Lymphovenous shunts in the treatment of lymphedema. Journal of the Chinese Medical Association, 2024.',
-  'Primary surgical prevention of lymphedema. Journal of the Chinese Medical Association, 2024.',
-  'Genital Elephantiasis: Surgical treatment and reconstruction. Journal of the Chinese Medical Association, 2024.',
-  'Vascular anatomy of the anteromedial thigh flap. JPRAS Open, 2017.'
+  'Primary surgical prevention of lymphedema. Journal of the Chinese Medical Association, 2024;87(6):567–571.',
+  'Genital elephantiasis: Surgical treatment and reconstruction. Journal of the Chinese Medical Association, 2024;87(2):142–147.',
+  'Vascular anatomy of the anteromedial thigh flap. JPRAS Open, 2017;13:113–125.'
 ];
 
 const publicationSources: Record<string, string> = {
+  'Utilizing the Subunit Concept to Achieve Better Outcomes in Lower Limb Reconstruction: A Clinical Experience in an Asian Population. PRS Global Open, 2024;12(4):e5752.': 'https://doi.org/10.1097/GOX.0000000000005752',
+  'Primary surgical prevention of lymphedema. Journal of the Chinese Medical Association, 2024;87(6):567–571.': 'https://doi.org/10.1097/JCMA.0000000000001101',
+  'Genital elephantiasis: Surgical treatment and reconstruction. Journal of the Chinese Medical Association, 2024;87(2):142–147.': 'https://doi.org/10.1097/JCMA.0000000000001021',
+  'Vascular anatomy of the anteromedial thigh flap. JPRAS Open, 2017;13:113–125.': 'https://doi.org/10.1016/j.jpra.2017.05.008',
   'The 1+X technique: A novel microsurgical approach for secondary lymphedema. JPRAS, 2025;111:1–3.': 'https://doi.org/10.1016/j.bjps.2025.10.006',
   'Venous anatomy of the superficial circumflex iliac artery perforator flap: a cadaveric and clinical study. JPRAS, 2024.': 'https://pubmed.ncbi.nlm.nih.gov/37948880/',
   'Current Insights into Post-Traumatic Lymphedema. Trauma Care, 2025.': 'https://doi.org/10.3390/traumacare5040024',
